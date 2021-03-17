5d3e407edff421eb7afe8d1821cb05db7148b32f
deyvisson.souza <deyvisson_souza01@hotmail.com>
2021-03-17 02:23:30 -0300

    Formato base do sistema
    Definição do formato adotado para armazenamento base da aplicação

--- src/main.c
+++ src/main.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main(){
+    printf("                                 SISTEMA DE GERENCIAMENTO DE VENDAS           \n"); 
+    printf("                                         SEJA BEM-VINDO(A)!                   \n\n");
+    printf("                                TABELA DE VENDA DOS FUNCIONÁRIOS                   \n\n");
+
+    printf("CÓDIGO DA VENDA | CÓDIGO DO VENDEDOR | CÓDIGO DO PRODUTO | CLIENTE |    DATA    | QUANTIDADE | VALOR FINAL\n");
+}
