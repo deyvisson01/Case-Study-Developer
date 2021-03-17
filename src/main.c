a6d590cb1fc9b73bb1df29d41b1ad2f6996796c2
deyvisson.souza <deyvisson_souza01@hotmail.com>
2021-03-17 02:36:33 -0300

    Estrutura base
    Criação da estrutura base do menu inicial, onde o usuário irá selecionar qual seu cargo entre vendedor ou administrador.

--- src/main.c
+++ src/main.c
@@ -2,9 +2,33 @@
 #include <stdlib.h>
 
 int main(){
+    // VARIÁVEIS
+    int opt;
+
+
     printf("                                 SISTEMA DE GERENCIAMENTO DE VENDAS           \n"); 
     printf("                                         SEJA BEM-VINDO(A)!                   \n\n");
     printf("                                TABELA DE VENDA DOS FUNCIONÁRIOS                   \n\n");
 
     printf("CÓDIGO DA VENDA | CÓDIGO DO VENDEDOR | CÓDIGO DO PRODUTO | CLIENTE |    DATA    | QUANTIDADE | VALOR FINAL\n");
+
+    printf("\nOPÇÕES DE GERENCIAMENTO:\n");
+    printf("1 - Área do vendedor\n");
+    printf("2 - Área da administração\n");
+
+    printf("\nEntre com o identificador desejado:");
+    scanf("%i", &opt);
+
+    switch (opt)
+        {
+            case 1:
+                
+            break;
+
+            case 2:
+                
+            break;
+            default :
+                printf ("ERROR: ENTRADA INVÁLIDA!\n");
+        }
 }
